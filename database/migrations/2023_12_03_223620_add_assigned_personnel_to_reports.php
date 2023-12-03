73404fda769c64c46c30239e40204aac2f35beb5
Jasper Jhon D. Anotde <anotdejasper2@gmail.com>
2023-12-03 22:44:02 +0800

    Add migration file for Reports

---
+++ database/migrations/2023_12_03_223620_add_assigned_personnel_to_reports.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->unsignedBigInteger('assigned_personnel_id')->nullable();
+            $table->foreign('assigned_personnel_id')->references('id')->on('personnels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropForeign(['assigned_personnel_id']);
+            $table->dropColumn('assigned_personnel_id');
+        });
+    }
+};
